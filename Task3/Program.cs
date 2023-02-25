9124fc087a6dbf1995270ab01d3ede14acb174b0
Leonid Tolstov <leonski@mail.ru>
2023-02-25 11:49:36 +0300

    task 10 mass

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -8,3 +8,12 @@
 [1, 2, 3, 6, 2]-> 0
 
 [10, 11, 12, 13, 14]-> 5*/
+
+var fibonacciNumbers = new List<int> {1, 1, 2, 3, 5, 8, 13, 21};
+var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 4];
+
+fibonacciNumbers.Add(previous + previous2);
+
+foreach(var item in fibonacciNumbers)
+    Console.WriteLine(item);

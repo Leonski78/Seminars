280401dfa98e8c998579049e91097a398ad6c986
Leonid Tolstov <leonski@mail.ru>
2023-03-03 13:03:59 +0300

    Zad 46-51

---
+++ Zad41/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+// Console.Write("Введите числа через пробелы: ");
+// // Console.WriteLine(String.Join(", ", array));
+// string[] st = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+// st = new string[st.Length];
+// int count = 0;
+
+// for (int i=0; i<st.Length; i++)
+// {
+//     if (st[i] > 0) 
+//     count++;
+// }
+// Console.WriteLine($"количество чисел > 0 равно: {count}");
+
+int Prompt (string message)
+{
+Console.Write(message);
+string value = Console.ReadLine();
+int result = Convert.ToInt32(value);
+return result;
+}
+int[] InputArray(int length)
+{
+    int[] array = new int [length];
+    for (int i=0; i<array.Length; i++)
+    {
+        array [i] = Prompt($"Введите {i + 1}-й элемент ");
+    }
+    return array;
+}
+void PrintArray (int[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.WriteLine($"а[{i}] = {array [i]}");
+    }
+}
+int CountPositiveNumbers(int[]array)
+{
+    int count = 0;
+    for (int i=0; i<array.Length; i++)
+    {
+        if (array [i]>0)
+        {
+            count ++;
+        }
+    }
+    return count;
+}
+int leng = Prompt ("Введите количество элементов >");
+int [] array;
+array = InputArray(leng);
+PrintArray(array);
+Console.WriteLine($"Колиество чисел больше 0 = {CountPositiveNumbers(array)}");
+
+/*2-й вариант
+using System;
+using static System.Console;
+
+Write("Введите числа через пробел: ");
+int[] numbers = GetArrayFromString(ReadLine());
+WriteLine($"Количество чисел больше 0 –> {GetCountPositiveElements(numbers)}");
+WriteLine();
+
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[numS.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = int.Parse(numS[i]);
+    }
+    return result;
+}
+
+int GetCountPositiveElements(int[] array)
+{
+    int count = 0;
+    foreach (var item in array)
+    {
+        if(item>0) count++;
+    }
+    return count;
+}*/
---
+++ Zad48/Program.cs
@@ -0,0 +1,68 @@
+﻿// **Задача 48:** Задайте 
+// двумерный массив размера m на n, каждый элемент
+//  в массиве находится по формуле: A[i][j] = i + j. 
+//Выведите полученный массив на экран.
+
+// m = 3, n = 4.
+
+// 0 1 2 3
+
+// 1 2 3 4
+
+// 2 3 4 5
+
+// int NewMass (int [,] array)
+// {
+//     int res = 0;
+//     int max = array.GetLength(1);
+//     if(array.GetLength(0) < array.GetLength(1))
+//     {
+//     max = array.GetLength(0);
+//     }
+    
+//     for (int i = 0; i < max; i++)
+//     {
+//         //for (int j = 1; j < array.GetLength(1); j+=2)
+//         //{
+//           res+= array[i,i];
+//         //}
+//     }
+//     return res;
+// }
+
+using System;
+using static System.Console;
+Clear();
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество строк массива: ");
+int colums = int.Parse(ReadLine());
+
+int[,]array = GetArray(rows, colums);
+PrintArray(array);
+
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
